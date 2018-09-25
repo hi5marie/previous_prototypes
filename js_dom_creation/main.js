47e3216f64cf079f67e80f823e2544baf0d82c11
mterrado <mcterrado@me.com>
2018-09-25 13:54:39 -0700

    JS | DOM Creation - Marie Terrado

--- js_dom_creation/main.js
+++ js_dom_creation/main.js
@@ -2,15 +2,61 @@ $(document).ready(initializeApp);
 
 function initializeApp(){
     console.log("Initializing App...");
-    var dynamicCol = $('.container .col:nth-child(2)').append('<h1>Dynamic Content</h1>');
-}
+    var dynamicCol = $('.container .col:nth-child(2)');
+    dynamicCol.append('<h1>Dynamic Content</h1>');
+    var firstCard = createCard('heart', 'ten');
+    dynamicCol.append(firstCard);
+    var firstRandomCard = createRandomCard();
+    dynamicCol.append(firstRandomCard);
+    dynamicCol.append(addRandomCardToDom);
 
-//var dynamicCol = $('.container .col:nth-child(2)');
-//dynamicCol.append('<h1>Dynamic Content</h1>');
+    var addCardButton = $('<button>', {
+        text: 'Add Card to DOM',
+        click: addRandomCardToDom
+    });
+    dynamicCol.prepend(addCardButton);
+}
 
 function createCard(suit, rank){
     var card = $('<div>', {
-    class: 'card '+ suit + ' ' + rank
+    class: 'card '+ suit + ' ' + rank,
+        click: toggleCardBack
     });
+    return card;
+}
 
+function createRandomCard(){
+    var suitArray = ['heart', 'club', 'diamond', 'spade'];
+    var rankArray = [
+        'ace',
+        'two',
+        'three',
+        'four',
+        'five',
+        'six',
+        'seven',
+        'eight',
+        'nine',
+        'ten',
+        'jack',
+        'queen',
+        'king'
+    ];
+    var suitIndex = Math.floor(Math.random()) * suitArray.length;
+    var rankIndex = Math.floor(Math.random()) * rankArray.length;
+    var randomSuit = suitArray[suitIndex];
+    var randomRank = rankArray[rankIndex];
+    var randomCard = createCard(randomSuit, randomRank);
+    return randomCard;
+}
+
+function addRandomCardToDom(){
+    var randomCard = createRandomCard();
+    $('.container .col:nth-child(2)').append(randomCard);
+
+}
+
+function toggleCardBack(){
+    var clickedCard = $(this);
+    clickedCard.toggleClass('back');
 }
